f8dd735eff05014805915268f5ed67e0d341ad17
Samir Ferreira <samir.vaferreira@gmail.com>
2020-11-24 01:59:02 -0300

    Initial commit

---
+++ Assets/BlockSpawner.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+
+public class BlockSpawner : MonoBehaviour
+{
+    public Transform[] spawnPoints;
+
+    public GameObject blockPrefab;
+
+    private void Start()
+    {
+        int randomIndex = Random.Range(0, spawnPoints.Length);
+
+        for (int i = 0; i < spawnPoints.Length; i++)
+        {
+            if (randomIndex != i)
+            {
+                Instantiate(blockPrefab, spawnPoints[i].position, Quaternion.identity);
+            }
+        }
+    }
+}
---
+++ Assets/Player.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    public float speed = 15f;
+
+    private float mapWidth = 5f;
+    private Rigidbody2D rb;
+
+    private void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    private void FixedUpdate()
+    {
+        float x = Input.GetAxis("Horizontal") * Time.fixedDeltaTime * speed;
+
+        Vector2 newPosition = rb.position + Vector2.right * x;
+
+        newPosition.x = Mathf.Clamp(newPosition.x, -mapWidth, mapWidth);
+
+        rb.MovePosition(newPosition);
+    }
+}
